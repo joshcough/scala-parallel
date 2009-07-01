68f9666db61aa71c1f0e8ad253bff4be84b683b1
Josh Cough <joshcough@gmail.com>
2009-07-01 09:13:09 -0400

    added scalatest and first test, which is basically empty

---
+++ src/test/scala/scala/util/concurrent/locks/PimpedReadWriteLockTest.scala
@@ -0,0 +1,15 @@
+package scala.util.concurrent.locks
+
+import java.util.concurrent.locks.{Lock, ReadWriteLock}
+import org.scalatest.FunSuite
+import org.scalatest.matchers.MustMatchers
+
+/**
+ *
+ * @author Josh Cough
+ * Date: Jun 30, 2009
+ * Time: 8:42:43 AM
+ */
+class PimpedReadWriteLockTest extends FunSuite with MustMatchers {
+  test("nothing"){ println("hey") }
+}
