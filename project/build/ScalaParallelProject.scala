f38ced2d482440ad424783c4515054bc30d8dce8
Nathan Hamblen <nathan@technically.us>
2009-07-07 20:06:11 -0400

    cross compile with sbt 2.5.1

--- project/build/ScalaParallelProject.scala
+++ project/build/ScalaParallelProject.scala
@@ -1,6 +1,8 @@
 import sbt._
 
 class ScalaParallelProject(info: ProjectInfo) extends DefaultProject(info) with AutoCompilerPlugins {
+  override def crossScalaVersions = Set("2.7.2", "2.7.3", "2.7.4", "2.7.5")
+
   val st = "org.scala-tools.testing" % "scalatest" % "0.9.5" % "test->default"
 
 }
