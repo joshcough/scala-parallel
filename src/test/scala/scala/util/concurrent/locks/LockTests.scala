ea02a6a6417f21aca831065c5ca3a593a4a41f4f
joshcough <joshcough@gmail.com>
2009-07-10 01:41:41 -0400

    added example usage with 3 locks

--- src/test/scala/scala/util/concurrent/locks/LockTests.scala
+++ src/test/scala/scala/util/concurrent/locks/LockTests.scala
@@ -82,7 +82,8 @@ class MultiLockTest extends FunSuite with MustMatchers with MustBeSugar{
 
   val lockA = new JReentrantLock
   val lockB = new JReentrantLock
-    
+  val lockC = new JReentrantLock
+  
   test("lock two locks with and"){
     val answer: Int = (lockA and lockB) { 42 }
     answer mustBe 42
@@ -92,4 +93,15 @@ class MultiLockTest extends FunSuite with MustMatchers with MustBeSugar{
     val answer: Int = for( a <- lockA; b <- lockB ) yield 42
     answer mustBe 42
   }
+
+  test("lock three locks with and"){
+    val answer: Int = (lockA and lockB and lockC) { 42 }
+    answer mustBe 42
+  }
+
+  test("lock three locks with for"){
+    val answer: Int = for( a <- lockA; b <- lockB; c <- lockC ) yield { 42 }
+    answer mustBe 42
+  }
+
 }
